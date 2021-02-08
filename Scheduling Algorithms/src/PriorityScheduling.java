ef95f7bc415410ae84fe7eb1436d42999fce6282
Nimish Dadlani <nmd2611@gmail.com>
2021-02-08 10:37:40 +0530

    Priority no premptive done, Gant chart remaining

--- Scheduling Algorithms/src/PriorityScheduling.java
+++ Scheduling Algorithms/src/PriorityScheduling.java
@@ -6,21 +6,22 @@ public class PriorityScheduling {
     public static Process []p;
 
     public static void inputProcess(int n){
-        int i,priority;
+        Scanner sc = new Scanner(System.in);
+        int i;
         for(i=0;i<n;i++)
         {
             System.out.println("Process P" + (i+1) + ":");
 
             p[i] = new Process();
-            p[i].createProcess(i+1, "priority");		
+            p[i].ProcessName = i;
+
+            System.out.print("Enter Burst Time : ");
+            p[i].BurstTime = sc.nextInt();
+            
+            System.out.print("Enter priority : ");
+            p[i].priority = sc.nextInt();
         }
 
-        //sorting processes in ascending order of arrival time
-        Arrays.sort(p, new Comparator<Process>() {
-            public int compare(Process idx1, Process idx2) {
-                return idx1.ArrivalTime - idx2.ArrivalTime; 
-            }
-        });
     }
 
 
@@ -79,8 +80,5 @@ public class PriorityScheduling {
 
         nonPremptive(n);
 
-        // double awt=0.0,att=0.0;
-        // int currentTime = 0;
-        // int i;
     }
 }
